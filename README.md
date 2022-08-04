067f4d7ae0d8cee8fc60c274b2c28d8fd7e1b310
David Wallace Croft <david@CroftSoft.com>
2022-08-03 21:26:21 -0500

    periodic savings needed

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 - An adaption to Rust of the Java-based CroftSoft Core Library
 - http://www.croftsoft.com/library/code/
 - Published to crates.io on 2022-07-30
+
+## Documentation
+
+- cargo doc --open
--- src/math/finance_lib/mod.rs
+++ src/math/finance_lib/mod.rs
@@ -1,6 +1,6 @@
 // =============================================================================
 //! - Financial calculations
-//! - Rust version: 2022-07-30
+//! - Rust version: 2022-08-03
 //! - Rust since: 2022-07-30
 //! - Adapted from the Java class com.croftsoft.core.math.FinanceLib
 //! - <http://www.croftsoft.com/library/code/>
@@ -9,13 +9,9 @@
 // =============================================================================
 
 // -----------------------------------------------------------------------------
-/// Calculates the annual savings necessary to accumulate a specified
-/// value in the future
-///
-/// - `f` - Future value desired
-/// - `r` - Annual interest
-/// - `t` - Number of years of savings
+/// Replaced by periodic_savings_needed()
 // -----------------------------------------------------------------------------
+#[deprecated(since = "0.2.0", note = "Replaced by periodic_savings_needed()")]
 pub fn annual_savings_needed(
   f: f64,
   r: f64,
@@ -23,3 +19,56 @@ pub fn annual_savings_needed(
 ) -> f64 {
   f * r / ((1.0 + r).powf(t) - 1.0)
 }
+
+#[derive(Clone, Copy, Debug)]
+pub struct PeriodicSavingsNeeded {
+  pub future_value: f64,
+  pub interest_rate: f64,
+  pub time_periods: f64,
+  // TODO: Maybe a private memoization field
+}
+
+impl PeriodicSavingsNeeded {
+  pub fn calculate(&self) -> f64 {
+    // TODO: Is this moving self into the method?
+    periodic_savings_needed(*self)
+  }
+}
+
+// -----------------------------------------------------------------------------
+/// Calculates the periodic savings necessary to accumulate a specified
+/// value in the future
+///
+/// - `f` - Future value desired
+/// - `r` - Periodic interest rate (use 0.01 for 1%)
+/// - `t` - Number of time periods of investment
+///
+/// # Example
+/// ```
+/// use com_croftsoft_core::math::finance_lib::*;
+/// assert_eq!(
+///   periodic_savings_needed(PeriodicSavingsNeeded {
+///     future_value:  1_000_000.0,   // To have a million dollars in the future
+///     interest_rate: 0.12,          // At 12% interest compounded annually
+///     time_periods:  10.0,          // Investing each year for ten years
+///   }),
+///   56_984.164_159_844_026);        // You would need to invest ~$57k per year
+/// assert_eq!(
+///   PeriodicSavingsNeeded {
+///     future_value:  100_000_000.0, // To have a hundred million cents
+///     interest_rate: 0.01,          // At 1% interest compounded monthly
+///     time_periods:  120.0,         // Investing each month for 120 months
+///   }.calculate(),
+///   434_709.484_025_873_1); // Invest ~$435k cents per month (~$52k per year)
+/// ```
+// -----------------------------------------------------------------------------
+pub fn periodic_savings_needed(input: PeriodicSavingsNeeded) -> f64 {
+  let PeriodicSavingsNeeded {
+    future_value: f,
+    interest_rate: r,
+    time_periods: t,
+  } = input;
+  f * r / ((1.0 + r).powf(t) - 1.0)
+}
+
+// TODO: Add unit tests
