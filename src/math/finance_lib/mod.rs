551f9169f05afed738b8d80d2315b55f6459758d
David Wallace Croft <david@CroftSoft.com>
2022-08-19 21:33:14 -0500

    internal rate of return

--- src/math/finance_lib/mod.rs
+++ src/math/finance_lib/mod.rs
@@ -1,6 +1,6 @@
 // =============================================================================
 //! - Financial calculations
-//! - Rust version: 2022-08-17
+//! - Rust version: 2022-08-19
 //! - Rust since: 2022-07-30
 //! - Adapted from the Java class com.croftsoft.core.math.FinanceLib
 //! - <https://www.croftsoft.com/library/code/>
@@ -89,6 +89,72 @@ impl FutureValuePaymentStream {
 }
 
 // -----------------------------------------------------------------------------
+/// The calculated discount rate where the net present value is zero.
+///
+/// # Example
+/// ```
+/// use com_croftsoft_core::math::finance_lib::InternalRateOfReturn;
+/// assert_eq!(
+///   InternalRateOfReturn {
+///     cash_flows:   &vec![-2.0, 1.1, 1.2], // -$2 now, $1.1 in 1y, $1.2 in 2y
+///     irr_estimate: 0.10,                  // Estimated IRR of 10%
+///   }.calculate(),
+///   0.09696411113867977);                  // Calculated IRR ~9.7%
+/// ```
+// -----------------------------------------------------------------------------
+#[derive(Clone, Debug)]
+pub struct InternalRateOfReturn<'a> {
+  /// Array of cash flows received in the future, indexed from time zero
+  pub cash_flows: &'a Vec<f64>,
+  /// The initial estimated value for the IRR (use 0.10 for 10%)
+  pub irr_estimate: f64,
+}
+
+impl<'a> InternalRateOfReturn<'a> {
+  pub fn calculate(&self) -> f64 {
+    let mut irr: f64 = self.irr_estimate;
+    let mut delta = -irr * 0.1;
+    let mut old_npv = 0.0;
+    loop {
+      let npv: f64 = NetPresentValue {
+        cash_flows: self.cash_flows,
+        discount_rate: irr,
+      }
+      .calculate();
+      if npv == 0.0 {
+        return irr;
+      }
+      if old_npv < 0.0 {
+        if npv > 0.0 {
+          delta *= -0.9;
+        } else if npv > old_npv {
+          delta *= 1.1;
+        } else if npv < old_npv {
+          delta = -delta;
+        } else {
+          delta = 0.0;
+        }
+      } else if old_npv > 0.0 {
+        if npv < 0.0 {
+          delta *= -0.9;
+        } else if npv < old_npv {
+          delta *= 1.1;
+        } else if npv > old_npv {
+          delta = -delta;
+        } else {
+          delta = 0.0;
+        }
+      }
+      if delta == 0.0 {
+        return irr;
+      }
+      irr += delta;
+      old_npv = npv;
+    }
+  }
+}
+
+// -----------------------------------------------------------------------------
 /// The discounted value of multiple cash flows received in the future.
 ///
 /// # Example
@@ -96,38 +162,39 @@ impl FutureValuePaymentStream {
 /// use com_croftsoft_core::math::finance_lib::NetPresentValue;
 /// assert_eq!(
 ///   NetPresentValue {
-///     cash_flows:    vec![1.0], // A dollar today
-///     discount_rate: 0.10,      // At a discount rate of 10% per time period
+///     cash_flows:    &vec![1.0], // A dollar today
+///     discount_rate: 0.10,       // At a discount rate of 10% per time period
 ///   }.calculate(),
-///   1.0);                       // Is worth a dollar today
+///   1.0);                        // Is worth a dollar today
 /// assert_eq!(
 ///   NetPresentValue {
-///     cash_flows:    vec![0.0, 1.0], // A dollar next year
+///     cash_flows:    &vec![0.0, 1.0], // A dollar next year
+///     discount_rate: 0.10,            // At a discount rate of 10% per year
+///   }.calculate(),
+///   0.9090909090909091);              // Is worth ~$0.91 today
+/// assert_eq!(
+///   NetPresentValue {
+///     cash_flows:    &vec![0.0, 0.0, 1.0], // A dollar received in two years
+///     discount_rate: 0.10,                 // Discounted at 10% per year
+///   }.calculate(),
+///   0.8264462809917354);                   // Is worth ~$0.83 today
+/// assert_eq!(
+///   NetPresentValue {
+///     cash_flows:    &vec![1.0; 11], // $1 today plus $1 per year for 10 years
 ///     discount_rate: 0.10,           // At a discount rate of 10% per year
-///   }.calculate(),
-///   0.9090909090909091);             // Is worth ~$0.91 today
-/// assert_eq!(
-///   NetPresentValue {
-///     cash_flows:    vec![0.0, 0.0, 1.0], // A dollar received in two years
-///     discount_rate: 0.10,                // Discounted at 10% per year
-///   }.calculate(),
-///   0.8264462809917354);                  // Is worth ~$0.83 today
-/// assert_eq!(
-///   NetPresentValue {
-///     cash_flows:    vec![1.0; 11], // $1 today plus $1 each year for 10 years
-///     discount_rate: 0.10,          // At a discount rate of 10% per year
 ///   }.calculate(),
 ///   7.144567105704681);              // Is worth ~$7.14 today
 /// ```
 // -----------------------------------------------------------------------------
-pub struct NetPresentValue {
+#[derive(Clone, Debug)]
+pub struct NetPresentValue<'a> {
   // Cash flows received in the future indexed from time zero
-  pub cash_flows: Vec<f64>,
+  pub cash_flows: &'a Vec<f64>,
   /// The discount rate or cost of capital (use 0.01 for 1%)
   pub discount_rate: f64,
 }
 
-impl NetPresentValue {
+impl<'a> NetPresentValue<'a> {
   pub fn calculate(&self) -> f64 {
     let mut net_present_value = 0.0;
     for (index, cash_flow) in self.cash_flows.iter().enumerate() {
@@ -215,6 +282,7 @@ impl PeriodicSavingsNeeded {
 ///   0.8264462809917354);   // Would be worth the same as ~$0.83 invested today
 /// ```
 // -----------------------------------------------------------------------------
+#[derive(Clone, Copy, Debug)]
 pub struct PresentValue {
   // Cash flow received in the future
   pub cash_flow: f64,
@@ -241,38 +309,39 @@ impl PresentValue {
 /// use com_croftsoft_core::math::finance_lib::PresentValueCashFlows;
 /// assert_eq!(
 ///   PresentValueCashFlows {
-///     cash_flows:    vec![1.0],  // A dollar received one year in the future
-///     discount_rate: 0.10,       // With inflation at 10% per year
+///     cash_flows:    &vec![1.0],  // A dollar received one year in the future
+///     discount_rate: 0.10,        // With inflation at 10% per year
 ///   }.calculate(),
 ///   0.9090909090909091);   // Will have the spending power of ~$0.91 today
 /// assert_eq!(
 ///   PresentValueCashFlows {
-///     cash_flows:    vec![0.0, 1.0],  // A dollar received in two years
-///     discount_rate: 0.10,            // With inflation at 10% per year
+///     cash_flows:    &vec![0.0, 1.0],  // A dollar received in two years
+///     discount_rate: 0.10,             // With inflation at 10% per year
 ///   }.calculate(),
 ///   0.8264462809917354);   // Would be worth the same as ~$0.83 invested today
 /// assert_eq!(
 ///   PresentValueCashFlows {
-///     cash_flows:    vec![1.0, 2.0, 3.0], // $1, $2, and $3 over 3 years
-///     discount_rate: 0.0,                 // With no inflation
+///     cash_flows:    &vec![1.0, 2.0, 3.0], // $1, $2, and $3 over 3 years
+///     discount_rate: 0.0,                  // With no inflation
 ///   }.calculate(),
-///   6.0);                                 // Would be worth $6 today
+///   6.0);                                  // Would be worth $6 today
 /// assert_eq!(
 ///   PresentValueCashFlows {
-///     cash_flows:    vec![1.0, 2.0, 3.0], // $1, $2, and $3 over 3 years
-///     discount_rate: 0.10,                // With inflation at 10% per year
+///     cash_flows:    &vec![1.0, 2.0, 3.0], // $1, $2, and $3 over 3 years
+///     discount_rate: 0.10,                 // With inflation at 10% per year
 ///   }.calculate(),
-///   4.8159278737791125);                  // Would be worth ~$4.82 today
+///   4.8159278737791125);                   // Would be worth ~$4.82 today
 /// ```
 // -----------------------------------------------------------------------------
-pub struct PresentValueCashFlows {
+#[derive(Clone, Debug)]
+pub struct PresentValueCashFlows<'a> {
   // Cash flows received in the future starting one time period from now
-  pub cash_flows: Vec<f64>,
+  pub cash_flows: &'a Vec<f64>,
   /// The discount rate or inflation rate per time period (use 0.01 for 1%)
   pub discount_rate: f64,
 }
 
-impl PresentValueCashFlows {
+impl<'a> PresentValueCashFlows<'a> {
   pub fn calculate(&self) -> f64 {
     self.cash_flows.iter().enumerate().fold(0.0, |sum, (index, cash_flow)| {
       sum
