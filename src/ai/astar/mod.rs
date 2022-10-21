0239563fe021b50e3d7aeeb280c3183fb012e2fb
David Wallace Croft <david@CroftSoft.com>
2022-10-21 06:47:49 -0500

    AI module

---
+++ src/ai/astar/mod.rs
@@ -0,0 +1,26 @@
+// =============================================================================
+//! - A* algorithm for path planning
+//! - This module is documented in the book [`Advanced Java Game Programming`]
+//!
+//! # Metadata
+//! - Copyright: &copy; 2022 [`CroftSoft Inc`]
+//! - Author: [`David Wallace Croft`]
+//! - Rust version: 2022-10-21
+//! - Rust since: 2022-10-21
+//!
+//! # History
+//! - Adapted from the Java package com.croftsoft.core.ai.astar
+//!   - In the Java-based [`CroftSoft Core Library`]
+//!
+//! [`Advanced Java Game Programming`]: https://croftsoft.com/library/books/ajgp
+//! [`CroftSoft Core Library`]: https://www.croftsoft.com/library/code/
+//! [`CroftSoft Inc`]: https://www.croftsoft.com/
+//! [`David Wallace Croft`]: https://www.croftsoft.com/people/david/
+// =============================================================================
+
+// pub mod functions;
+// pub mod methods;
+// pub mod operations;
+// pub mod structures;
+pub mod traits;
+// pub mod trait_imps;
---
+++ src/ai/astar/traits/mod.rs
@@ -0,0 +1,52 @@
+// =============================================================================
+//! - Traits for the A* algorithm
+//!
+//! # Metadata
+//! - Copyright: &copy; 2002 - 2022 [`CroftSoft Inc`]
+//! - Author: [`David Wallace Croft`]
+//! - Rust version: 2022-10-21
+//! - Rust since: 2022-10-21
+//! - Java version: 2003-04-29
+//! - Java since: 2002-04-21
+//!
+//! # History
+//! - Adapted from the interfaces in the Java-based [`CroftSoft Core Library`]
+//!   - com.croftsoft.core.ai.astar.Cartographer
+//!   - com.croftsoft.core.ai.astar.SpaceTester
+//!
+//! [`CroftSoft Core Library`]: https://www.croftsoft.com/library/code/
+//! [`CroftSoft Inc`]: https://www.croftsoft.com/
+//! [`David Wallace Croft`]: https://www.croftsoft.com/people/david/
+// =============================================================================
+
+/// An A* algorithm map maker
+pub trait Cartographer<N> {
+  fn estimate_cost_to_goal(
+    &self,
+    node: N,
+  ) -> f64;
+
+  fn get_adjacent_nodes(
+    &self,
+    node: N,
+  ) -> Vec<N>;
+
+  fn get_cost_to_adjacent_node(
+    &self,
+    from_node: N,
+    to_node: N,
+  ) -> f64;
+
+  fn is_goal_node(
+    &self,
+    node: N,
+  ) -> bool;
+}
+
+/// Tests whether a point in space is available as an adjacent node
+pub trait SpaceTester<P> {
+  fn is_space_available(
+    &self,
+    point_xy: P,
+  ) -> bool;
+}
